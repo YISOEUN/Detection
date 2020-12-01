b97beeb30a63cb6083969d988844e46bb3649ff3
yerin1106 <51941176+yerin1106@users.noreply.github.com>
2020-12-01 14:15:45 +0900

    Update hybridDetection.py

--- hybridDetection.py
+++ hybridDetection.py
@@ -1,17 +1,30 @@
 import warnings
+
 warnings.filterwarnings(action='ignore')
 
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
-from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.model_selection import KFold, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve
+# ROC curve function
+def rocvis(true , prob , label ) :
+    from sklearn.metrics import roc_curve, auc
+    fpr, tpr, thresholds = roc_curve(true, prob)
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr, tpr, marker='.', label = label)
+    print(label,roc_auc)
 
 # -------------------------------------------------------------------------------------------------------------
 # misuse detection
@@ -20,15 +33,16 @@ misuse_df = pd.read_csv(misuse_file)
 print(misuse_df)
 
 # fill nan
-misuse_df = misuse_df.replace({'AttackType':"NA"},{'AttackType':"Normal"})
+misuse_df = misuse_df.replace({'AttackType': "NA"}, {'AttackType': "Normal"})
 misuse_df = misuse_df.fillna('Normal')
 
 misuse_x = misuse_df.drop('AttackType', axis=1)
 misuse_y = misuse_df['AttackType'].values
 
 # anomaly detection
-anomaly_file = 'C:/Users/samsung/Desktop/testData.csv'
+anomaly_file = 'C:/Users/samsung/Desktop/Dataset_Anomaly_AttributeSelection.csv'
 anomaly_df = pd.read_csv(anomaly_file)
+anomaly_df = anomaly_df.drop('Unnamed: 0',axis = 1)
 print(anomaly_df)
 
 # Data encoding
@@ -43,15 +57,14 @@ anomaly_x = anomaly_df.drop('class', axis=1)
 anomaly_y = anomaly_df['class'].values
 
 # Read test data
-test_file = 'C:/Users/samsung/Desktop/Dataset_Anomaly_AttributeSelection.csv'
+test_file = 'C:/Users/samsung/Desktop/testData.csv'
 test_df = pd.read_csv(test_file)
 print(test_df)
+test_y = test_df['AttackType'].values
 test_df = test_df.drop('AttackType', axis=1)
 
 # Scale the data
-# Scaling - MinMaxScaler, Normalizer, StandardScaler
 MinMax_scaler = MinMaxScaler()
-Standard_scaler = StandardScaler()
 test_X = MinMax_scaler.fit_transform(test_df)
 
 # -------------------------------------------------------------------------------------------------------------
@@ -73,7 +86,7 @@ randomForest_params = {
 }
 
 gradient_params = {
-    "n_estimators": [50,100],
+    "n_estimators": [50, 100],
     "max_depth": [1, 2, 4],
     "learning_rate": [0.01, 0.1]
 }
@@ -101,6 +114,16 @@ print('best score', gcv_logistic.best_score_)  # best score
 logistic_best = gcv_logistic.best_estimator_
 gcv_logistic_score = gcv_logistic.best_score_
 
+index = 0
+result_lr = list(logistic_best.predict(test_X))
+for i in range(len(result_lr)):
+    if result_lr[i] != 'Normal':
+        result_lr[i] = 'Attack'
+
+# Confusion Matrix
+print(confusion_matrix(test_y, result_lr))
+print(classification_report(test_y, result_lr, target_names=['Attack', 'Normal']))
+
 # Decision Tree
 gcv_decisionTree = GridSearchCV(decisionTree, param_grid=decisionTree_params, scoring='accuracy', cv=cv, verbose=1)
 gcv_decisionTree.fit(X_scaled, misuse_y)
@@ -110,6 +133,16 @@ print('final params', gcv_decisionTree.best_params_)  # print best parameter val
 print('best score', gcv_decisionTree.best_score_)  # best score
 gcv_decisionTree_score = gcv_decisionTree.best_score_
 decisionTree_best = gcv_decisionTree.best_estimator_
+
+index = 0
+result_dt = list(decisionTree_best.predict(test_X))
+for i in range(len(result_dt)):
+    if result_dt[i] != 'Normal':
+        result_dt[i] = 'Attack'
+
+# Confusion Matrix
+print(confusion_matrix(test_y, result_dt))
+print(classification_report(test_y, result_dt, target_names=['Attack', 'Normal']))
 
 # Random Forest
 gcv_randomForest = GridSearchCV(randomForest, param_grid=randomForest_params, scoring='accuracy', cv=cv, verbose=1)
@@ -121,6 +154,16 @@ print('best score', gcv_randomForest.best_score_)  # best score
 gcv_randomForest_score = gcv_randomForest.best_score_
 randomForest_best = gcv_randomForest.best_estimator_
 
+index = 0
+result_rf = list(decisionTree_best.predict(test_X))
+for i in range(len(result_rf)):
+    if result_rf[i] != 'Normal':
+        result_rf[i] = 'Attack'
+
+# Confusion Matrix
+print(confusion_matrix(test_y, result_rf))
+print(classification_report(test_y, result_rf, target_names=['Attack', 'Normal']))
+
 # Gradient Boosting
 gcv_gradientBoosting = GridSearchCV(gradientBoosting, param_grid=gradient_params, scoring='accuracy', cv=cv, verbose=1)
 gcv_gradientBoosting.fit(X_scaled, misuse_y)
@@ -130,12 +173,20 @@ print('final params', gcv_gradientBoosting.best_params_)  # print best parameter
 print('best score', gcv_gradientBoosting.best_score_)  # best score
 gradientBoosting_best = gcv_gradientBoosting.best_estimator_
 gcv_gradientBoosting_score = gcv_gradientBoosting.best_score_
-gradientBoosting_predict = gradientBoosting_best.predict(test_X)
-print(gradientBoosting_predict)
+
+index = 0
+result_gb = list(gradientBoosting_best.predict(test_X))
+for i in range(len(result_gb)):
+    if result_gb[i] != 'Normal':
+        result_gb[i] = 'Attack'
+
+# Confusion Matrix
+print(confusion_matrix(test_y, result_gb))
+print(classification_report(test_y, result_gb, target_names=['Attack', 'Normal']))
 
 # Find best model & score
 score = [gcv_decisionTree_score, gcv_randomForest_score, gcv_gradientBoosting_score]
-models = [decisionTree_best, randomForest_best,gradientBoosting_best]
+models = [decisionTree_best, randomForest_best, gradientBoosting_best]
 max_score = gcv_logistic_score
 best_model = logistic_best
 for i in range(len(score)):
@@ -143,19 +194,47 @@ for i in range(len(score)):
         max_score = score[i]
         best_model = models[i]
 
-print("Misuse detection best score: ",max_score)
+print("Misuse detection best score: ", max_score)
 
 # a new dataframe by collecting only normal results through best model
 normal_X = pd.DataFrame(columns=test_df.columns)
-
+normal_y = []
 index = 0
 result = list(best_model.predict(test_X))
 for i in range(len(result)):
     if result[i] == 'Normal':
         normal_X.loc[index] = test_X[i]
+        normal_y.append(test_y[i])
         index = index + 1
+    else:
+        print('index', i, '---->', result[i])
+        result[i] = 'Attack'
+
+normal_y = np.array(normal_y)
+
+# Confusion Matrix
+print(confusion_matrix(test_y, result))
+print(classification_report(test_y, result, target_names=['Attack', 'Normal']))
 
 print(normal_X)
+print(normal_y)
+
+# ROC curve
+test_y = encoder.fit_transform(test_y)
+result_lr = encoder.fit_transform(result_lr)
+result_dt = encoder.fit_transform(result_dt)
+result_rf = encoder.fit_transform(result_rf)
+result_gb = encoder.fit_transform(result_gb)
+
+plt.plot([0, 1], [0, 1], linestyle='--')
+rocvis(test_y, result_lr,"Logistic Regression")
+rocvis(test_y, result_dt,"Decision Tree")
+rocvis(test_y, result_rf,"Random Forest")
+rocvis(test_y, result_gb,"Gradient Boosting")
+
+plt.legend(fontsize = 18)
+plt.title("Models Roc Curve" , fontsize= 25)
+plt.show()
 
 # -------------------------------------------------------------------------
 print("Anomaly detection")
@@ -180,6 +259,12 @@ print('best score', gcv_logistic.best_score_)  # best score
 logistic_best = gcv_logistic.best_estimator_
 gcv_logistic_score = gcv_logistic.best_score_
 
+index = 0
+result_lr = list(logistic_best.predict(normal_X))
+# Confusion Matrix
+print(confusion_matrix(normal_y, result_lr))
+print(classification_report(normal_y, result_lr, target_names=['Attack', 'Normal']))
+
 # Decision Tree
 gcv_decisionTree = GridSearchCV(decisionTree, param_grid=decisionTree_params, scoring='accuracy', cv=cv, verbose=1)
 gcv_decisionTree.fit(X_scaled, anomaly_y)
@@ -189,6 +274,12 @@ print('final params', gcv_decisionTree.best_params_)  # print best parameter val
 print('best score', gcv_decisionTree.best_score_)  # best score
 decisionTree_best = gcv_decisionTree.best_estimator_
 gcv_decisionTree_score = gcv_decisionTree.best_score_
+
+index = 0
+result_dt = list(decisionTree_best.predict(normal_X))
+# Confusion Matrix
+print(confusion_matrix(normal_y, result_dt))
+print(classification_report(normal_y, result_dt, target_names=['Attack', 'Normal']))
 
 # Random Forest
 gcv_randomForest = GridSearchCV(randomForest, param_grid=randomForest_params, scoring='accuracy', cv=cv, verbose=1)
@@ -200,6 +291,12 @@ print('best score', gcv_randomForest.best_score_)  # best score
 randomForest_best = gcv_randomForest.best_estimator_
 gcv_randomForest_score = gcv_randomForest.best_score_
 
+index = 0
+result_rf = list(randomForest_best.predict(normal_X))
+# Confusion Matrix
+print(confusion_matrix(normal_y, result_rf))
+print(classification_report(normal_y, result_rf, target_names=['Attack', 'Normal']))
+
 # Gradient Boosting
 gcv_gradientBoosting = GridSearchCV(gradientBoosting, param_grid=gradient_params, scoring='accuracy', cv=cv, verbose=1)
 gcv_gradientBoosting.fit(X_scaled, anomaly_y)
@@ -210,9 +307,15 @@ print('best score', gcv_gradientBoosting.best_score_)  # best score
 gradientBoosting_best = gcv_gradientBoosting.best_estimator_
 gcv_gradientBoosting_score = gcv_gradientBoosting.best_score_
 
+index = 0
+result_gb = list(randomForest_best.predict(normal_X))
+# Confusion Matrix
+print(confusion_matrix(normal_y, result_gb))
+print(classification_report(normal_y, result_gb, target_names=['Attack', 'Normal']))
+
 # best model & score
-score = [gcv_decisionTree_score, gcv_randomForest_score,gcv_gradientBoosting_score]
-models = [decisionTree_best, randomForest_best,gradientBoosting_best]
+score = [gcv_decisionTree_score, gcv_randomForest_score, gcv_gradientBoosting_score]
+models = [decisionTree_best, randomForest_best, gradientBoosting_best]
 max_score = gcv_logistic_score
 best_model = logistic_best
 for i in range(len(score)):
@@ -220,10 +323,31 @@ for i in range(len(score)):
         max_score = score[i]
         best_model = models[i]
 
-print("Anomaly detection best score: ",max_score)
+print("Anomaly detection best score: ", max_score)
 
 result = list(best_model.predict(normal_X))
-for i in range(len(result)) :
-    if result[i] == 'anomaly' :
-        print('index',i,'---->',result[i])
+for i in range(len(result)):
+    if result[i] == 'anomaly':
+        print('index', i, '---->', result[i])
         print(normal_X.iloc[i])
+
+# Confusion Matrix
+print(confusion_matrix(normal_y, result))
+print(classification_report(normal_y, result, target_names=['Attack', 'Normal']))
+
+# ROC curve
+normal_y = encoder.fit_transform(normal_y)
+result_lr = encoder.fit_transform(result_lr)
+result_dt = encoder.fit_transform(result_dt)
+result_rf = encoder.fit_transform(result_rf)
+result_gb = encoder.fit_transform(result_gb)
+
+plt.plot([0, 1], [0, 1], linestyle='--')
+rocvis(test_y, result_lr,"Logistic Regression")
+rocvis(test_y, result_dt,"Decision Tree")
+rocvis(test_y, result_rf,"Random Forest")
+rocvis(test_y, result_gb,"Gradient Boosting")
+
+plt.legend(fontsize = 18)
+plt.title("Models Roc Curve" , fontsize= 25)
+plt.show()
