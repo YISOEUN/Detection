cf19ebc55a5f45cd3599c75335a34dbafa098907
KIM-JOOHYUN <64013256+KIM-JOOHYUN@users.noreply.github.com>
2020-12-11 23:22:52 +0900

    set_column
    - python code to set data columns

---
+++ set_column.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+#기준이 되는 Data set
+pre = 'Dataset_Misuse_AttributeSelection.csv'
+#column 정리가 필요한 data file
+test = 'KDDTrain_plus.csv'
+#다시 저장할 file 이름
+restore = 'train_plus.csv'
+
+#read csv files
+a_df = pd.read_csv(pre)
+b_df = pd.read_csv(test)
+##b_df = b_df.drop('Unnamed: 0',axis = 1)
+
+a_col = a_df.columns
+b_col = b_df.columns
+
+#column 정리
+for i in b_col :
+    if not i in a_col  and not i == 'class':
+##check
+##        print(i)
+        b_df = b_df.drop(i,axis = 1)
+    if i == 'class' :
+        b_df.rename(columns = {"class": "AttackType"}, inplace = True)
+
+b_col = b_df.columns
+a_col = a_df.columns
+## check
+##for i in a_col :
+##    if not i in b_col :
+##        print(i)
+
+#chcek
+print(a_df.columns)
+print()
+print(b_df.columns)
+
+#check
+for i in range(len(a_col)) :
+    if a_col[i] == b_col[i] :
+        print(i,'.',a_col[i],"  ", b_col[i])
+    else :
+         print(i,'.',a_col[i],"  ", b_col[i])
+         break
+
+#모든 column이 있으면 다시 저장
+if len(b_col ) == 37 :
+    b_df.to_csv(restore)
+    print('store complete -',restore)
+
+
